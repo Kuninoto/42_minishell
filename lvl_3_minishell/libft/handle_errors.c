0cce02bca8f809b0465ccb2832dc464030a9a4c7
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-12-10 02:52:53 +0000

    general skeleton

---
+++ lvl_3_minishell/libft/handle_errors.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_errors.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/27 23:27:32 by nnuno-ca          #+#    #+#             */
+/*   Updated: 2022/11/28 17:54:03 by nnuno-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	handle_errors(char *error_msg)
+{
+	write(STDERR_FILENO, "Error: ", 7);
+	write(STDERR_FILENO, error_msg, ft_strlen(error_msg));
+	write(STDERR_FILENO, "\n", 1);
+	exit(EXIT_FAILURE);
+}
