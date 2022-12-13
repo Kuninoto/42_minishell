8dec1df88905077f4df632596a341bb3eefd9dfd
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-12-13 01:03:18 +0000

    More links, phew

--- lvl_3_minishell/srcs/parser.c
+++ lvl_3_minishell/srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 19:51:02 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/12/13 00:20:30 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/12/13 00:38:25 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,28 +58,3 @@ char	**parse_input(char *input)
 
 	cmd ::= text
 */
-
-/* static size_t	get_table_len(char **splitted)
-{
-	size_t	counter;
-	
-	counter = 0;
-	while ()
-	{
-		
-		counter++;
-	}
-}
-
-t_simple_command *parse_input(char *input)
-{
-	char	*temp;
-	t_simple_command *table;
-
-	temp = ft_split(input, ' ');
-	table = malloc(get_table_len(temp) * sizeof(t_simple_command));
-
-	
-
-	return (table);
-} */
