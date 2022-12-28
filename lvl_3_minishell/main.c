6b2328672f97e43b13767a9653c768a2aa2d1823
ramos21rodrigo <ramos03rodrigo@gmail.com>
2022-12-28 20:10:57 +0000

    cmd_check_singles

--- lvl_3_minishell/main.c
+++ lvl_3_minishell/main.c
@@ -3,21 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: roramos <roramos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 02:02:08 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/12/23 20:23:33 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/12/28 20:09:59 by roramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
+int			g_exit_status = 0;
+
 
 void	dismiss_signal(int signum)
 {
 	if (signum == SIGINT)
 	{
 		rl_replace_line("", 1);
-		write(STDIN_FILENO, "\n$ ", 3);
+			write(STDIN_FILENO, "\n$ ", 3);
 	}
 }
 
@@ -78,12 +80,14 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_vector	var_vec;
 	t_vector	envp_vec; //exported vars
+	int			g_exit_status;
 	char		*raw_input;
 	char		*input;
 	t_statement	*statement_list;
 
 	(void)argc;
 	(void)argv;
+	g_exit_status = 0;
 	var_vec = vec_new();
 	envp_vec = vec_new();
 	config_signals();
@@ -110,50 +114,22 @@ int	main(int argc, char **argv, char **envp)
 			free_vec(&var_vec);
 			exit(EXIT_SUCCESS);
 		}
-		statement_list = parse_input(input, &var_vec, &envp_vec);
-		free(input);
-
-/* 		for (t_statement *temp = statement_list; temp != NULL; temp = temp->next)
-		{
-			printf("ARGC: %d\n", temp->argc);
-			printf("ARGV: ");
-			for (size_t i = 0; temp->argv[i] != NULL; i++)
-				printf("%s ", temp->argv[i]);
-			printf("\nOPERATOR: %d\n", temp->operator);
-			printf("\n");
-		}
-		printf("\n"); */
-		
+		statement_list = parse_input(input, &var_vec, &envp_vec, g_exit_status);
+		free(input);		
 		if (lstsize(statement_list) == 1)
 		{
-			if (streq(statement_list->argv[0], "unset"))
-			{
-				if (is_onvec(statement_list->argv[1], &envp_vec))
-					vec_pop_at(statement_list->argv[1], &envp_vec);
-			}
-			else if (streq(statement_list->argv[0], "export"))
-				cmd_export(&envp_vec, &var_vec, statement_list->argv[1]);
-			else if (streq(statement_list->argv[0], "cd"))
-				cmd_cd(statement_list->argv[1]);
-			else if (ft_strchr(statement_list->argv[0], '='))
-				save_user_vars(statement_list->argv[0], &var_vec);
-			else if (fork() == 0)
+			if (!cmd_check_singles(statement_list, &envp_vec, &var_vec) && fork() == 0)
 			{
 				exec_cmd(statement_list, envp, &envp_vec);
 				exit(EXIT_SUCCESS);
 			}
-			wait(&g_exit_status);
-			if (WIFEXITED(g_exit_status))
-				g_exit_status = WEXITSTATUS(g_exit_status);
 		}
 		else
-		{
 			if (fork() == 0)
-				exec_cmd(statement_list, envp, &envp_vec);
-			wait(&g_exit_status);
-			if (WIFEXITED(g_exit_status))
-				g_exit_status = WEXITSTATUS(g_exit_status);
-		}
+				exec_cmd(statement_list , envp, &envp_vec);
+		wait(&g_exit_status);
+		if (WIFEXITED(g_exit_status))
+			g_exit_status = WEXITSTATUS(g_exit_status);
 		lstclear(&statement_list);
 	}
 	return (EXIT_SUCCESS);
--- lvl_3_minishell/srcs/commands/checks.c
+++ lvl_3_minishell/srcs/commands/checks.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check.c                                            :+:      :+:    :+:   */
+/*   checks.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: roramos <roramos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 21:53:21 by roramos           #+#    #+#             */
-/*   Updated: 2022/12/22 19:36:23 by roramos          ###   ########.fr       */
+/*   Updated: 2022/12/28 20:04:12 by roramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,24 @@ bool	cmd_check(t_statement *statement, char **envp, t_vector *envp_vec)
 		cmd_pwd();
 	else if (streq(statement->argv[0], "env"))
 		cmd_env(envp, envp_vec);
+	else
+		return (false);
+	return (true);
+}
+
+bool	cmd_check_singles(t_statement *statement, t_vector *envp_vec, t_vector *var_vec)
+{
+	if (streq(statement->argv[0], "unset"))
+	{
+		if (is_onvec(statement->argv[1], envp_vec))
+			vec_pop_at(statement->argv[1], envp_vec);
+	}
+	else if (streq(statement->argv[0], "export"))
+		cmd_export(envp_vec, var_vec, statement->argv[1]);
+	else if (streq(statement->argv[0], "cd"))
+		cmd_cd(statement->argv[1]);
+	else if (ft_strchr(statement->argv[0], '='))
+		save_user_vars(statement->argv[0], var_vec);
 	else
 		return (false);
 	return (true);
