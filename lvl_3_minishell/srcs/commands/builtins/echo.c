b3176655a536c24a264e3c6cb452cca1f0fe9c09
kuninoto <nnuno-ca@student.42porto.com>
2023-02-11 04:44:08 +0000

    echo refactor

--- lvl_3_minishell/srcs/commands/builtins/echo.c
+++ lvl_3_minishell/srcs/commands/builtins/echo.c
@@ -6,28 +6,26 @@
 /*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 14:57:53 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2023/02/11 03:18:48 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2023/02/11 04:43:05 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print(t_statement *statement, int i)
+static void	print(char *str)
 {
-	char	*line;
-	size_t	j;
-
-	line = statement->argv[i];
-	j = 0;
-	while (line[j])
+	size_t	i;
+	
+	i = 0;
+	while (str[i])
 	{
-		if (line[j] == '\\')
+		if (str[i] == '\\')
 		{
-			j += 1;
+			i += 1;
 			continue ;
 		}
-		ft_putchar_fd(line[j], STDOUT_FILENO);
-		j += 1;
+		ft_putchar_fd(str[i], STDOUT_FILENO);
+		i += 1;
 	}
 }
 
@@ -36,18 +34,15 @@ int	cmd_echo(t_statement *statement)
 	bool	has_n;
 	int		i;
 
-	has_n = streq(statement->argv[1], "-n");
 	i = 1;
+	if (statement->argc >= 2)
+		has_n = streq(statement->argv[1], "-n");
 	if (has_n)
-	{
-		if (!statement->argv[2])
-			return (EXIT_SUCCESS);
-		i = 2;
-	}
+		i += 1;
 	while (i != statement->argc)
 	{
-		print(statement, i);
-		if (statement->argv[i + 1])
+		print(statement->argv[i]);
+		if (i + 1 < statement->argc)
 			ft_putchar_fd(' ', STDOUT_FILENO);
 		i += 1;
 	}
