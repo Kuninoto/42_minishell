b344f72301a5fa3a553ba9693c96cf23aced097b
ramos21rodrigo <ramos03rodrigo@gmail.com>
2023-02-18 20:27:21 +0000

    eval fixs: missing echo user

--- lvl_3_minishell/srcs/commands/builtins.c
+++ lvl_3_minishell/srcs/commands/builtins.c
@@ -6,7 +6,7 @@
 /*   By: roramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 15:24:53 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2023/02/18 17:29:02 by roramos          ###   ########.fr       */
+/*   Updated: 2023/02/18 20:20:40 by roramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,40 @@
 
 extern int	g_exit_status;
 
-void	call_cmd_exit(t_statement *s, t_data *data)
+static bool	is_valid_id(char c)
 {
-	if (s->argc == 2)
-	{
-		if (is_all_digits(s->argv[1]))
-			cmd_exit(ft_atoi(s->argv[1]), data);
-		else
-			cmd_exit(2, data);
-	}
-	else if (s->argc > 2)
-		cmd_exit(EXIT_FAILURE, data);
+	if (is_digit(c) || c == '!' || c == '@'
+		|| c == '{' || c == '}')
+		return (false);
+	return (true);
+}
+
+static int	call_cmd_unset(t_statement *s, t_data *data)
+{
+	if (s->argc == 1)
+		return (EXIT_SUCCESS);
+	return (cmd_unset(s->argv[1], &data->envp_lst));
+}
+
+static int	call_cmd_cd(t_statement *s)
+{
+	if (s->argc > 2)
+		return (cd_too_many_args());
 	else
-		cmd_exit(EXIT_SUCCESS, data);
+		return (cmd_cd(s->argv[1]));
 }
 
 bool	builtin(t_statement *s, t_data *data)
 {
 	if (streq(s->argv[0], "exit"))
-		call_cmd_exit(s, data);
+		cmd_exit(s, data);
 	else if (streq(s->argv[0], "unset"))
-		g_exit_status = cmd_unset(s->argv[1], &data->envp_lst);
+		g_exit_status = call_cmd_unset(s, data);
 	else if (streq(s->argv[0], "export"))
 		g_exit_status = cmd_export(s, data);
 	else if (streq(s->argv[0], "cd"))
-	{
-		if (s->argc > 2)
-			g_exit_status = cd_too_many_args();
-		else
-			g_exit_status = cmd_cd(s->argv[1]);
-	}
-	else if (ft_strchr(s->argv[0], '=') && !is_digit(s->argv[0][0]))
+		g_exit_status = call_cmd_cd(s);
+	else if (ft_strchr(s->argv[0], '=') && !is_valid_id(s->argv[0][0]))
 		g_exit_status = save_user_vars(s->argv[0],
 				&data->envp_lst, false);
 	else if (streq(s->argv[0], "echo"))
--- lvl_3_minishell/srcs/commands/builtins/exit.c
+++ lvl_3_minishell/srcs/commands/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: roramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 18:17:34 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2023/02/11 13:29:32 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2023/02/18 20:20:59 by roramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 
 extern int	g_exit_status;
 
-void	cmd_exit(int exit_status, t_data *data)
+void	cmd_exit(t_statement *s, t_data *data)
 {
 	ft_putendl_fd("exit", STDOUT_FILENO);
+	if (s->argc == 2)
+	{
+		if (is_all_digits_or_signals(s->argv[1]))
+			g_exit_status = ft_atoi(s->argv[1]);
+		else
+		{
+			g_exit_status = 2;
+			ft_putendl_fd(EXIT_NON_NUMERIC_ARG, STDERR_FILENO);
+		}
+	}
+	else if (s->argc > 2)
+		ft_putendl_fd(EXIT_TOO_MANY_ARGS, STDERR_FILENO);
+	else
+		g_exit_status = EXIT_SUCCESS;
 	if (data)
 		destroy(data);
-	g_exit_status = exit_status;
-	if (exit_status == EXIT_FAILURE)
-		ft_putendl_fd(EXIT_TOO_MANY_ARGS, STDERR_FILENO);
-	else if (exit_status == 2)
-		ft_putendl_fd(EXIT_NON_NUMERIC_ARG, STDERR_FILENO);
 	exit(g_exit_status);
 }
