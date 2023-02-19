3607fb8a92f8b4e23e51dfa37d094ccccdf41661
kuninoto <nnuno-ca@student.42porto.com>
2023-02-19 19:17:13 +0000

    more syntax checks

--- lvl_3_minishell/srcs/utils/valid_input.c
+++ lvl_3_minishell/srcs/utils/valid_input.c
@@ -6,7 +6,7 @@
 /*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 13:31:56 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2023/02/19 17:46:57 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2023/02/19 19:08:00 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ bool	valid_input(char *input, t_data *data)
 		ft_putendl_fd(UNCLOSED_QUOTES, STDERR_FILENO);
 		valid = false;
 	}
-	else if (invalid_syntax(input) || invalid_syntax2(input) || invalid_syntax3(input))
+	else if (invalid_syntax(input) || invalid_syntax2(input))
 		valid = false;
 	if (!valid)
 	{
